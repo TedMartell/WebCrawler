1c85fdd69ddc8475b47727df10e53968acc65d51
TedMartell <martellted@gmail.com>
2024-05-18 00:39:52 +0300

    initial test up and running

--- crawl.js
+++ crawl.js
@@ -1,4 +1,10 @@
-async function normalizeURL() {
 
+
+
+function normalizeURL(url) {
+    const parsedURL = new URL(url);
+    const normalizedURL = parsedURL.hostname + parsedURL.pathname;
+    return normalizedURL;
 }
+
 export { normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -3,8 +3,8 @@ import { test, expect } from "@jest/globals";
 import { normalizeURL } from "./crawl.js";
 
 
-const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('https://blog.boot.dev/path/ to be blog.boot.dev/path', () => {
+    const result = normalizeURL('https://blog.boot.dev/path/');
+  expect(result).toBe('blog.boot.dev/path/');
 });
