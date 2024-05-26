ecf7616e618c28dd7440f1b2ca402ac13d06cc4a
TedMartell <martellted@gmail.com>
2024-05-26 12:30:38 +0300

    main updated

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+
 function main() {
-    // ?
+    if (process.argv.length < 3) {
+        console.log("No website provided")
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log("To many inputs")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`web crawler starting at ${baseURL}`)
+
+
   }
   
   main()
