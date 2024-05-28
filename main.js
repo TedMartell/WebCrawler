0a2aa579ac3023176f61ca3be17e46c7ee8f7327
TedMartell <martellted@gmail.com>
2024-05-28 19:29:46 +0300

    printreport, sortPages working

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { crawlPage } from "./crawl.js"
+import { printReport } from "./report.js"
 
 
 async function main() {
@@ -14,13 +15,9 @@ async function main() {
 
     console.log(`web crawler starting at ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-
-    for (const page of Object.entries(pages)) {
-        console.log(page)
+    printReport(pages)
     }
 
-
-  }
   
   main()
   
---
+++ report.test.js
@@ -0,0 +1,16 @@
+import { sortPages, printReport } from './report.js'
+import { test, expect } from '@jest/globals'
+
+test('sortPages', () => {
+  const input = {
+    'https://wagslane.dev/path':1,
+    'https://wagslane.dev':3
+  }
+  const actual = sortPages(input)
+  const expected = [
+    ['https://wagslane.dev', 3]
+    ['https://wagslane.dev/path', 1]
+  ]
+  expect(actual).toEqual(expected)
+})
+
