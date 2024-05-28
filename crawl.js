ea3d5d32dcde14a5c9fda2c57a0e14017205fd11
TedMartell <martellted@gmail.com>
2024-05-28 19:04:10 +0300

    crawls page recursive

--- crawl.js
+++ crawl.js
@@ -1,14 +1,31 @@
 import { JSDOM } from 'jsdom'
 
- async function crawlPage(currentURL) {
+ async function crawlPage(baseURL, currentURL, pages) {
+  
+
+  const baseURLObj = new URL(baseURL)
+  const currentURLObj = new URL(currentURL)
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages
+  }
+
+  const normalizedCurrentURL = normalizeURL(currentURL)
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++
+    return pages
+  }
+  pages[normalizedCurrentURL] = 1  
+
   console.log(`actively crawling: ${currentURL}`)
+
+
 
   try {
     const resp = await fetch(currentURL)
 
     if (resp.status >399) {
       console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
-      return
+      return pages
     }
 
     const contentType = resp.headers.get("content-type")
@@ -16,11 +33,18 @@ import { JSDOM } from 'jsdom'
       console.log(`none html response, content type: ${contentType}, on page ${currentURL}`)
     }
 
-    console.log(await resp.text())
+    const htmlBody = await resp.text()
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
 
   } catch (err) {
     console.log(`error in fetch ${currentURL}`)
   }
+  return pages
 
 
 }
